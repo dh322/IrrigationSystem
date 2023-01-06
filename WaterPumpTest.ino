caf5d0c087f7c061bc2a02d5fb5ee5b10e3a5633
dh322 <118651311+dh322@users.noreply.github.com>
2023-01-05 20:31:34 -0500

    Update WaterPumpTest.ino

--- WaterPumpTest.ino
+++ WaterPumpTest.ino
@@ -1,10 +1,11 @@
+// This was code written to test the functionality of the water pumps
 void setup() {
-  pinMode(2, OUTPUT);
+  pinMode(2, OUTPUT); // assign digital pin 2 to output signals
 }
 
 void loop() {
-  digitalWrite(2, HIGH);
-  delay(2000);
-  digitalWrite(2, LOW);
+  digitalWrite(2, HIGH); // turn on water pump for 2 seconds
+  delay(2000); 
+  digitalWrite(2, LOW); // turn off water pump for 2 seconds
   delay(2000);
 }
