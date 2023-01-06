fd5860fb93e75993b6899eedd61bca4fc0069ae8
dh322 <118651311+dh322@users.noreply.github.com>
2023-01-05 20:23:59 -0500

    Update IrrigationSystemMain.ino

--- IrrigationSystemMain.ino
+++ IrrigationSystemMain.ino
@@ -1,29 +1,32 @@
+// Initializing variables
 int sensor1;
 int sensor2;
-int waterPump1 = 1;
-int waterPump2 = 2;
+int waterPump1 = 1; // assign to digital pin 1
+int waterPump2 = 2; // assign to digital pin 2
 
 void setup() {
-  serial.begin(9600);
-  pinMode(waterPump1, OUTPUT);
+  serial.begin(9600); // set baud rate to 9600
+  pinMode(waterPump1, OUTPUT); // assign to output signals
   pinMode(waterPump2, OUTPUT);
 }
 
 void loop() {
-  sensor1 = analogRead(A0);
-  if (sensor1 > 268) {
-    digitalWrite(waterPump1, HIGH);
+  sensor1 = analogRead(A0); // assign to receive signals from analog pin 0
+  if (sensor1 > 268) { // if value is greater than 268
+    digitalWrite(waterPump1, HIGH); // associated water pump turns on for 10 seconds
     delay(10000);
-    digitalWrite(waterPump1, LOW);
+    digitalWrite(waterPump1, LOW); // associated water pump turns off
   }
-  sensor2 = analogRead(A1);
-  if (sensor 2 > 268) {
+  // subsequent sensors and pumps follow the same process
+  sensor2 = analogRead(A1); // assign to receive signals from analog pin 1
+  if (sensor2 > 268) {
     digitalWrite(waterPump2, High);
     delay(10000);
     digitalWrite(waterPump2, LOW);
   }
-  delay(5000)
+  delay(5000) // pause for 5 seconds, this is to allow the water to diffuse throughout the pot
 }
 
+// these values were tested using MoistureSensorTest
 // when exposed to air, value is 464
 // when exposed to water, value is 196
