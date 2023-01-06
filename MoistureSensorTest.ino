a2c009ddb8074b11d0d8b196d5dc0aed37a04a55
dh322 <118651311+dh322@users.noreply.github.com>
2023-01-05 20:28:21 -0500

    Update MoistureSensorTest.ino

--- MoistureSensorTest.ino
+++ MoistureSensorTest.ino
@@ -1,13 +1,14 @@
+// initialize sensor
 int sensor1;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // set baud rate to 9600
 }
 
 void loop() {
-  sensor1 = analogRead(A0);
-  Serial.println(sensor1);
-  delay(1000);
+  sensor1 = analogRead(A0); // assign to receive signals from analog pin 1
+  Serial.println(sensor1); // print value of sensor in serial monitor
+  delay(1000); // pause for 1 second, to slow down the rate of values that will be printed
 }
 
 // when exposed to air, value is 464
